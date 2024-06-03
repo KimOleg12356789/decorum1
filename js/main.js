fbdb82f1fe7dd3e6302f0fdc1aa5e2e72c7f1d2b
Jango <88618558+KimOleg12356789@users.noreply.github.com>
2024-06-03 16:34:05 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,107 +1,6 @@
 $(document).ready(function(){
 
-jQuery(function($) {
-  //play video btn
-  $(".position-relative video").removeAttr("controls"); //hide controls by default
-  //on clicking play button
-  $(".video-section").each(function() {
-    $(".play-btn").click(function() {
-      var video = $(this)
-        .closest(".position-relative")
-        .find("video")
-        .get(0);
-      video.play();
-      video.controls = true;
-      $(this).css("visibility", "hidden");
-      return false;
-    });
 
-    //when video is paused
-    $(this)
-      .find("video")
-      .click(function() {
-        var video = $(this).get(0);
-        video.pause();
-        video.controls = false;
-        $(this)
-          .siblings(".play-btn")
-          .css("visibility", "visible");
-        return false;
-      });
-
-    //when video has ended
-    $(this)
-      .find("video")
-      .on("ended", function() {
-        $(this).get(0).controls = false;
-        $(this)
-          .siblings(".play-btn")
-          .css("visibility", "visible");
-        $(this)
-          .get(0)
-          .load();
-      });
-  });
- });
-
-
-
-
-
- // tabbed content
-    // http://www.entheosweb.com/tutorials/css/tabs.asp
-    $(".tab_content").hide();
-    $(".tab_content:first").show();
-
-  /* if in tab mode */
-    $("ul.tabs li").click(function() {
-    
-      $(".tab_content").hide();
-      var activeTab = $(this).attr("rel"); 
-      $("#"+activeTab).fadeIn();    
-    
-      $("ul.tabs li").removeClass("active");
-      $(this).addClass("active");
-
-    $(".tab_drawer_heading").removeClass("d_active");
-    $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
-    
-    });
-  /* if in drawer mode */
-  $(".tab_drawer_heading").click(function() {
-      
-      $(".tab_content").hide();
-      var d_activeTab = $(this).attr("rel"); 
-      $("#"+d_activeTab).fadeIn();
-    
-    $(".tab_drawer_heading").removeClass("d_active");
-      $(this).addClass("d_active");
-    
-    $("ul.tabs li").removeClass("active");
-    $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
-    });
-  
-  
-  /* Extra class "tab_last" 
-     to add border to right side
-     of last tab */
-  $('ul.tabs li').last().addClass("tab_last");
-
-
-
-  $("#menu").on("click","a", function (event) {
-        //отменяем стандартную обработку нажатия по ссылке
-        event.preventDefault();
-
-        //забираем идентификатор бока с атрибута href
-        var id  = $(this).attr('href'),
-
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top;
-        
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1000);
-});
 
 
 
@@ -426,7 +325,61 @@ jQuery(function($) {
 
 
 
+  
+  // tabbed content
+      // http://www.entheosweb.com/tutorials/css/tabs.asp
+      $(".tab_content").hide();
+      $(".tab_content:first").show();
 
+    /* if in tab mode */
+      $("ul.tabs li").click(function() {
+      
+        $(".tab_content").hide();
+        var activeTab = $(this).attr("rel"); 
+        $("#"+activeTab).fadeIn();    
+      
+        $("ul.tabs li").removeClass("active");
+        $(this).addClass("active");
+
+      $(".tab_drawer_heading").removeClass("d_active");
+      $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+      
+      });
+    /* if in drawer mode */
+    $(".tab_drawer_heading").click(function() {
+        
+        $(".tab_content").hide();
+        var d_activeTab = $(this).attr("rel"); 
+        $("#"+d_activeTab).fadeIn();
+      
+      $(".tab_drawer_heading").removeClass("d_active");
+        $(this).addClass("d_active");
+      
+      $("ul.tabs li").removeClass("active");
+      $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+      });
+    
+    
+    /* Extra class "tab_last" 
+      to add border to right side
+      of last tab */
+    $('ul.tabs li').last().addClass("tab_last");
+
+
+
+    $("#menu").on("click","a", function (event) {
+          //отменяем стандартную обработку нажатия по ссылке
+          event.preventDefault();
+
+          //забираем идентификатор бока с атрибута href
+          var id  = $(this).attr('href'),
+
+          //узнаем высоту от начала страницы до блока на который ссылается якорь
+              top = $(id).offset().top;
+          
+          //анимируем переход на расстояние - top за 1500 мс
+          $('body,html').animate({scrollTop: top}, 1000);
+  });
 
   $("select").change(function() {
     $(this).find("option:selected").each(function() {
@@ -439,6 +392,10 @@ jQuery(function($) {
       }
     });
   }).change();
+
+
+
+
 });
 
 
@@ -456,35 +413,35 @@ burger.addEventListener('click', () => {
 
 
 let triggersModal = document.querySelectorAll(".js-show-modal");
-      let modals = document.querySelectorAll(".js-modal");
+let modals = document.querySelectorAll(".js-modal");
 
-      function closeModal() {
-        modals.forEach(modal => {
-          if (modal.classList.contains("is-open")) {
-            modal.classList.remove("is-open");
-          }
-        });
-      }
+function closeModal() {
+  modals.forEach(modal => {
+    if (modal.classList.contains("is-open")) {
+      modal.classList.remove("is-open");
+    }
+  });
+}
 
 
-      modals.forEach(modal => {
-        modal.addEventListener("click", function (event) {
-          const isOutside = !event.target.closest(".modal__inner");
-          const isCloseButton = event.target.matches(".js-close-modal");
-          if (isCloseButton || isOutside) {
-            closeModal();
-          }
-        });
-      });
+modals.forEach(modal => {
+  modal.addEventListener("click", function (event) {
+    const isOutside = !event.target.closest(".modal__inner");
+    const isCloseButton = event.target.matches(".js-close-modal");
+    if (isCloseButton || isOutside) {
+      closeModal();
+    }
+  });
+});
 
-      triggersModal.forEach((button) =>
-      button.addEventListener("click", function (e) {
-        e.preventDefault();
-        let modalID = this.dataset.modal;
-        console.log(modalID);
-        modals.forEach(function (modal) {
-          if (modal.dataset.modal == modalID) {
-            modal.classList.add("is-open");
-          }
-        });
-      }));
+triggersModal.forEach((button) =>
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  let modalID = this.dataset.modal;
+  console.log(modalID);
+  modals.forEach(function (modal) {
+    if (modal.dataset.modal == modalID) {
+      modal.classList.add("is-open");
+    }
+  });
+}));
