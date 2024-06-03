a282d5457eb134f86a8750ff699423f2ef75e707
Jango <88618558+KimOleg12356789@users.noreply.github.com>
2024-06-03 16:43:05 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,60 @@
 $(document).ready(function(){
 
 
+ // tabbed content
+    // http://www.entheosweb.com/tutorials/css/tabs.asp
+    $(".tab_content").hide();
+    $(".tab_content:first").show();
+
+  /* if in tab mode */
+    $("ul.tabs li").click(function() {
+    
+      $(".tab_content").hide();
+      var activeTab = $(this).attr("rel"); 
+      $("#"+activeTab).fadeIn();    
+    
+      $("ul.tabs li").removeClass("active");
+      $(this).addClass("active");
+
+    $(".tab_drawer_heading").removeClass("d_active");
+    $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+    
+    });
+  /* if in drawer mode */
+  $(".tab_drawer_heading").click(function() {
+      
+      $(".tab_content").hide();
+      var d_activeTab = $(this).attr("rel"); 
+      $("#"+d_activeTab).fadeIn();
+    
+    $(".tab_drawer_heading").removeClass("d_active");
+      $(this).addClass("d_active");
+    
+    $("ul.tabs li").removeClass("active");
+    $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+    });
+  
+  
+  /* Extra class "tab_last" 
+     to add border to right side
+     of last tab */
+  $('ul.tabs li').last().addClass("tab_last");
+
+
+
+  $("#menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1000);
+});
 
 
 
@@ -324,63 +378,6 @@ $(document).ready(function(){
   });
 
 
-
-  
-  // tabbed content
-      // http://www.entheosweb.com/tutorials/css/tabs.asp
-      $(".tab_content").hide();
-      $(".tab_content:first").show();
-
-    /* if in tab mode */
-      $("ul.tabs li").click(function() {
-      
-        $(".tab_content").hide();
-        var activeTab = $(this).attr("rel"); 
-        $("#"+activeTab).fadeIn();    
-      
-        $("ul.tabs li").removeClass("active");
-        $(this).addClass("active");
-
-      $(".tab_drawer_heading").removeClass("d_active");
-      $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
-      
-      });
-    /* if in drawer mode */
-    $(".tab_drawer_heading").click(function() {
-        
-        $(".tab_content").hide();
-        var d_activeTab = $(this).attr("rel"); 
-        $("#"+d_activeTab).fadeIn();
-      
-      $(".tab_drawer_heading").removeClass("d_active");
-        $(this).addClass("d_active");
-      
-      $("ul.tabs li").removeClass("active");
-      $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
-      });
-    
-    
-    /* Extra class "tab_last" 
-      to add border to right side
-      of last tab */
-    $('ul.tabs li').last().addClass("tab_last");
-
-
-
-    $("#menu").on("click","a", function (event) {
-          //отменяем стандартную обработку нажатия по ссылке
-          event.preventDefault();
-
-          //забираем идентификатор бока с атрибута href
-          var id  = $(this).attr('href'),
-
-          //узнаем высоту от начала страницы до блока на который ссылается якорь
-              top = $(id).offset().top;
-          
-          //анимируем переход на расстояние - top за 1500 мс
-          $('body,html').animate({scrollTop: top}, 1000);
-  });
-
   $("select").change(function() {
     $(this).find("option:selected").each(function() {
       var optionValue = $(this).attr("value");
@@ -394,6 +391,49 @@ $(document).ready(function(){
   }).change();
 
 
+  jQuery(function($) {
+    //play video btn
+    $(".position-relative video").removeAttr("controls"); //hide controls by default
+    //on clicking play button
+    $(".video-section").each(function() {
+      $(".play-btn").click(function() {
+        var video = $(this)
+          .closest(".position-relative")
+          .find("video")
+          .get(0);
+        video.play();
+        video.controls = true;
+        $(this).css("visibility", "hidden");
+        return false;
+      });
+  
+      //when video is paused
+      $(this)
+        .find("video")
+        .click(function() {
+          var video = $(this).get(0);
+          video.pause();
+          video.controls = false;
+          $(this)
+            .siblings(".play-btn")
+            .css("visibility", "visible");
+          return false;
+        });
+  
+      //when video has ended
+      $(this)
+        .find("video")
+        .on("ended", function() {
+          $(this).get(0).controls = false;
+          $(this)
+            .siblings(".play-btn")
+            .css("visibility", "visible");
+          $(this)
+            .get(0)
+            .load();
+        });
+    });
+  });
 
 
 });
